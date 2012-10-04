6418dd9dd50d3a0df533004a26f79a57ce9a6252
Tony Hoffman <thetonyhoffman@gmail.com>
2012-10-04 13:23:51 -0400

    Added precompiling of assets for blueprint for heroku

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -64,4 +64,13 @@ Fanbase::Application.configure do
   # Log the query plan for queries taking more than this (works
   # with SQLite, MySQL, and PostgreSQL)
   # config.active_record.auto_explain_threshold_in_seconds = 0.5
+  
+  #Stuff I added:
+  # this precompiles blueprint for heroku
+  config.assets.precompile += %w( blueprint/screen.css blueprint/print.css blueprint/ie.css )
+  
+  
+  
+  
+  
 end
