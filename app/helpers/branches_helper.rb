ef0e9b0b5696cfaef88b57da3582efe3d8176f63
Tony Hoffman <thetonyhoffman@gmail.com>
2012-10-07 14:25:30 -0400

    Refixed final add for fourth pick.

--- app/helpers/branches_helper.rb
+++ app/helpers/branches_helper.rb
@@ -29,7 +29,7 @@ module BranchesHelper
       if @last_live_user_branch.nil?
         @live_master_branch = master_branches[0]
         else
-        if master_branches.count <= @user_branches.count then p = master_branches.count-1 else p = @user_branches.count end
+        if @user_branches.count < master_branches.count then p = @user_branches.count else p = master.branches.count - 1 end
         @live_master_branch = master_branches[p]
       end
     end
