26b79311bac03acbd65e55b85ffc04df45dd7706
Tony Hoffman <thetonyhoffman@gmail.com>
2012-10-08 10:22:47 -0400

    Added list of mobile devices from Mobylette site into application controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,7 +5,61 @@ class ApplicationController < ActionController::Base
   mobylette_config do |config|
   config[:fallback_chains] = { mobile: [:mobile, :html] }
     config[:skip_xhr_requests] = false
-    config[:mobile_user_agents] = proc { %r{iphone}i }
+    # config[:mobile_user_agents] = proc { %r{iphone}i }
+    config[:mobile_user_agents] = proc { %r{palm
+                                          blackberry
+                                          nokia
+                                          phone
+                                          midp
+                                          mobi
+                                          symbian
+                                          chtml
+                                          ericsson
+                                          minimo
+                                          audiovox
+                                          motorola
+                                          samsung
+                                          telit
+                                          upg1
+                                          windows\ ce
+                                          ucweb
+                                          astel
+                                          plucker
+                                          x320
+                                          x240
+                                          j2me
+                                          sgh
+                                          portable
+                                          sprint
+                                          docomo
+                                          kddi
+                                          softbank
+                                          android
+                                          mmp
+                                          pdxgw
+                                          netfront
+                                          xiino
+                                          vodafone
+                                          portalmmm
+                                          sagem
+                                          mot-
+                                          sie-
+                                          ipod
+                                          up.b
+                                          webos
+                                          amoi
+                                          novarra
+                                          cdm
+                                          alcatel
+                                          pocket
+                                          ipad
+                                          iphone
+                                          mobileexplorer
+                                          mobile
+                                          maemo
+                                          fennec
+                                          silk}i }
+    
   end
   
   # has_mobile_fu
